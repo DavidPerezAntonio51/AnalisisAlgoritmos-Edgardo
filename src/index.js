800a4193e64e4ada26d6c1bd503ec94fa50714cf
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-05-11 00:34:51 -0500

    Cambio en librerias

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-var main = document.getElementById("main");
-console.log(window.screen.width);
-let sketch = function(p) {
-    p.setup = function(){
-      p.createCanvas(1080,720);
-      p.background(1);
-    }
-  };
-  new p5(sketch, main);
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-}
+var stage = new createjs.Stage("demoCanvas");
+var circle = new createjs.Shape();
+circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+circle.x = circle.y = 100;
+stage.addChild(circle);
+stage.update();
