a6219d1d36620d3d0574b5c1d4a8cee292214181
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-05-11 00:51:15 -0500

    pruebas de js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import 'bootstrap';
 var stage = new createjs.Stage("demoCanvas");
 var circle = new createjs.Shape();
 circle.graphics.beginFill("red").drawCircle(0, 0, 40);
 circle.x = circle.y = 100;
 stage.addChild(circle);
 stage.update();
+var btn = document.getElementById("button1")
+const onClick = () =>{
+  console.log("click")
+}
+btn.onclick=onClick;
