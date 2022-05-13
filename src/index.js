552480b413a55ee9c89ed08cfcc087748a69d0c1
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-05-12 19:00:05 -0500

    script limpio, solo usarse para interfaz

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import 'bootstrap';
 import './css/styles.css'
-var stage = new createjs.Stage("demoCanvas");
-var circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-circle.x = circle.y = 100;
-stage.addChild(circle);
-stage.update();
