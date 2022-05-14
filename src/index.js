d14c2f54cb3fb79a6e44d40abf8647c8b406f6fb
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-05-14 13:49:50 -0500

    se crea encabezado responsivo de md a xxl

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import 'bootstrap';
 import './css/styles.css'
+import Ipn_Logo from './Assets/LOGO_POLI.png'
+import Escom_Logo from './Assets/LOGO_ESCOM.png';
+
+const logoIpn = document.createElement('img')
+const logoEscom = document.createElement('img')
+logoIpn.src = Ipn_Logo
+logoEscom.src = Escom_Logo
+logoIpn.classList.add("img-fluid")
+logoEscom.classList.add("img-fluid")
+document.getElementById("logo-escom").appendChild(logoEscom)
+document.getElementById("logo-ipn").appendChild(logoIpn)
