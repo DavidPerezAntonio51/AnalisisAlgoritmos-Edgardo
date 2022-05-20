1a24e238f5a9c29f7536b3bb401d76ab1db45597
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-05-20 00:12:38 -0500

    Se agregan elementos a home
    los elementos agregados estan dentro de un contenedor desmontable, se hara un nuevo montado segun lo que el usuario haga

---
+++ src/Componentes/Home.js
@@ -0,0 +1,37 @@
+import 'bootstrap';
+import '../css/styles.css';
+import crearFraja from './Franja';
+function Home(changeToSim) {
+    const contenedor = document.createElement('div');
+    const fila1 = document.createElement('div');
+    const fila2 = document.createElement('div');
+    const fila3 = document.createElement('div');
+    const col1 = document.createElement('div');
+    const col2 = document.createElement('div');
+    const col3 = document.createElement('div');
+    const botonSim = document.createElement('button');
+    botonSim.textContent = "Revisa Nuestro Simulador";
+    botonSim.classList.add("btn","btn-outline-bondi");
+    botonSim.onclick = changeToSim;
+    contenedor.classList.add("container-fluid");
+    fila1.classList.add("row","justify-content-center","align-content-center");
+    fila2.classList.add("row","justify-content-center","align-content-center");
+    col1.classList.add("col");
+    col2.classList.add("col");
+    col3.classList.add("col");
+    col1.appendChild(crearFraja("Problema de Corte de Varillas"));
+    col2.appendChild(crearFraja("Fuerza Bruta:"));
+    col3.appendChild(crearFraja("Solución Optima:"));
+    fila1.appendChild(col1);
+    fila2.appendChild(col2);
+    fila2.appendChild(col3);
+    fila3.appendChild(botonSim);
+    contenedor.appendChild(fila1);
+    contenedor.appendChild(fila2);
+    contenedor.appendChild(fila3);
+    contenedor.id="home";
+
+    return contenedor;
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import 'bootstrap';
 import './css/styles.css'
 import Ipn_Logo from './Assets/LOGO_POLI.png'
 import Escom_Logo from './Assets/LOGO_ESCOM.png';
+import Home from './Componentes/Home';
 
-const logoIpn = document.createElement('img')
-const logoEscom = document.createElement('img')
-logoIpn.src = Ipn_Logo
-logoEscom.src = Escom_Logo
-logoIpn.classList.add("img-fluid")
-logoEscom.classList.add("img-fluid")
-document.getElementById("logo-escom").appendChild(logoEscom)
-document.getElementById("logo-ipn").appendChild(logoIpn)
+/* Valores Constantes del DOM*/
+const bodyApp = document.getElementById("app");
+const logoIpn = document.createElement('img');
+const logoEscom = document.createElement('img');
+logoIpn.src = Ipn_Logo;
+logoEscom.src = Escom_Logo;
+logoIpn.classList.add("img-fluid");
+logoEscom.classList.add("img-fluid");
+document.getElementById("logo-escom").appendChild(logoEscom);
+document.getElementById("logo-ipn").appendChild(logoIpn);
+/*Variables en el DOM*/
+const changeToSim = () => {
+    bodyApp.remove();
+}
+const changeToHome = () => {
+    bodyApp.appendChild(inicio);
+}
+const inicio = Home(changeToSim)
+bodyApp.appendChild(inicio);
