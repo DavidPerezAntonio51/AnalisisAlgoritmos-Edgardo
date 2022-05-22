cd668dd78aa161394bedc5ccf2852fea805d9e35
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-05-22 16:58:02 -0500

    Se cambia a un archivo diferente

--- src/index.js
+++ src/index.js
@@ -1,39 +1,5 @@
-
-
 //import * as p5 from 'p5';
 //var Arr = [2,80,120]; 
-
-class RodCutting2
-{
-    static cutRod(price, n)
-    {
-        var max_val = Number.MIN_VALUE;
-        var val = Array(n + 1).fill(0);
-        val[0] = 0;
-        var valor = 0;
-        for (let i=1; i <= n; i++)
-        {
-            var max_val = Number.MIN_VALUE;
-            for (let j=0; j < i; j++)
-            {
-                valor = price[j] + val[i - j - 1];
-                if(valor > max_val){
-                  max_val=valor;
-                }
-            }
-            val[i] = max_val;
-        }
-        return val[n];
-    }
-    static main(args)
-    {
-        var arr = [1, 5, 8, 9, 10, 17, 17, 20];
-        var size = arr.length;
-        console.log("Maximo valor obtenido " + RodCutting2.cutRod(arr, size));
-    }
-}
-RodCutting2.main([]);
-
 /*
 const canvas = (sk) => {
   sk.setup = () => {
